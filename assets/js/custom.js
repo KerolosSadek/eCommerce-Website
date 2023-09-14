958c349cbbddf3ef8f8bb8f5875bc8407b08b10c
KerolosSadek <kokosadek55@gmail.com>
2023-09-14 16:14:58 +0300

    Add Payment Page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,19 @@
 fetch("https://blissofearth.abanoubgeorge.info/assets/js/currencies.json")
-.then(response => response.json())
-.then(currencies => {
-    const currencyMenu = document.getElementById("currencyMenu");
-    currencyMenu.innerHTML = ""; // Clear the "Loading..." message
-    for (const countryCode in currencies) {
-        const currency = currencies[countryCode];
-        const option = document.createElement("a");
-        option.classList.add("dropdown-item");
-        option.textContent = `${countryCode}: ${currency.symbol}`;
-        currencyMenu.appendChild(option);
-    }
-})
-.catch(error => {
-    console.error(error);
-});
+    .then(response => response.json())
+    .then(currencies => {
+        const currencyMenu = document.getElementById("currencyMenu");
+        currencyMenu.innerHTML = ""; // Clear the "Loading..." message
+        for (const countryCode in currencies) {
+            const currency = currencies[countryCode];
+            const option = document.createElement("a");
+            option.classList.add("dropdown-item");
+            option.textContent = `${countryCode}: ${currency.symbol}`;
+            currencyMenu.appendChild(option);
+        }
+    })
+    .catch(error => {
+        console.error(error);
+    });
 
 
 // Button to top
@@ -33,3 +33,32 @@ document.getElementById("btn-top").addEventListener("click", function () {
     document.documentElement.scrollTop = 0;
 });
 
+
+
+// Payment Page
+
+const paymentForm = document.getElementById("payment-form");
+const paymentMethodSelect = document.getElementById("payment-method");
+const paypalForm = document.getElementById("paypal-form");
+const stripeForm = document.getElementById("stripe-form");
+
+paymentMethodSelect.addEventListener("change", () => {
+    if (paymentMethodSelect.value === "paypal") {
+        paypalForm.style.display = "block";
+        stripeForm.style.display = "none";
+    } else if (paymentMethodSelect.value === "stripe") {
+        paypalForm.style.display = "none";
+        stripeForm.style.display = "block";
+    }
+});
+
+paymentForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const selectedPaymentMethod = paymentMethodSelect.value;
+
+    if (selectedPaymentMethod === "paypal") {
+        // Handle PayPal payment
+    } else if (selectedPaymentMethod === "stripe") {
+        // Handle Stripe payment
+    }
+});
